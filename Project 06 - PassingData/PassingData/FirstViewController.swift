0ca669cd091423eff925ca4ed65f61729c2834dd
Joshua Okoro <joshuaojc@gmail.com>
2019-02-21 08:58:27 -0600

    Renamed ViewController to FirstViewController and created a second view controller

--- Project 06 - PassingData/PassingData/FirstViewController.swift
+++ Project 06 - PassingData/PassingData/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  PassingData
 //
 //  Created by Joshua Okoro on 2/21/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Project 06 - PassingData/PassingData/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  PassingData
+//
+//  Created by Joshua Okoro on 2/21/19.
+//  Copyright © 2019 Joshua Okoro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
