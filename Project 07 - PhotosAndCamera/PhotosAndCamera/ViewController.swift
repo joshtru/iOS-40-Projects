c55db5c08568f37d4077e3760eab6039c8a05481
Joshua Okoro <joshuaojc@gmail.com>
2019-02-21 20:56:29 -0600

    Added functionality in controller

--- Project 07 - PhotosAndCamera/PhotosAndCamera/ViewController.swift
+++ Project 07 - PhotosAndCamera/PhotosAndCamera/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,12 +17,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var cameraImage: UIImageView!
     @IBOutlet weak var photoLibraryImage: UIImageView!
     
+    let imagePicker = UIImagePickerController()
 
     // MARK: - Interactions
     @IBAction func cameraButton(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        imagePicker.delegate = self
     }
     
     @IBAction func photoLibraryButton(_ sender: UIButton) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
+        imagePicker.delegate = self
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            photoLibraryImage.image = image
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
     
 }
