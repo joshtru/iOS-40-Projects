c96a86e26881f3822f46f246646f5d4bd2902acf
Joshua Okoro <joshuaojc@gmail.com>
2019-02-23 13:53:44 -0600

    Added Pull Request

--- Project 09 - PullToRefresh/PullToRefresh/ViewController.swift
+++ Project 09 - PullToRefresh/PullToRefresh/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    @IBOutlet weak var groceriesTable: UITableView!
+    
+    let pullRequest = UIRefreshControl()
     // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        
+        groceriesTable.addSubview(pullRequest)
+        pullRequest.backgroundColor = UIColor.gray
+        pullRequest.attributedTitle = NSAttributedString(string: "Refreshing")
+        pullRequest.addTarget(self, action: #selector(refreshTable), for: .valueChanged)
         
     }
+    
+    
+    // MARK: - Data
+    var list = ["Mango", "Banana", "Egg", "Fish", "Meat"]
+    
+    let cloudData = ["Milk", "Strawberry", "Plantain", "Peach"]
+    
+    // MARK: - Functions
+    @objc func refreshTable() {
+        let listSet = Set(list)
+        let cloudDataSet = Set(cloudData)
+        
+        if cloudDataSet.isSubset(of: listSet) {
+            pullRequest.endRefreshing()
+        } else {
+            list += cloudData
+            groceriesTable.reloadData()
+            pullRequest.endRefreshing()
+        }
+    }
+    
     // MARK: - TableView
     
     //Number of Sections
@@ -24,17 +54,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // Number of Rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return list.count
     }
     
     // Display Data to TableView Cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "groceriesCell", for: indexPath)
         
-        cell.textLabel?.text = "Hello"
+        cell.textLabel?.text = list[indexPath.row]
         
         return cell
     }
+    
 
 
 }
