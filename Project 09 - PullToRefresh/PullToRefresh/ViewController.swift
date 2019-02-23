69f1478c54f4c0dbc3c40be440b3674d46429c96
Joshua Okoro <joshuaojc@gmail.com>
2019-02-23 13:07:48 -0600

    Added necessary functions needed to display data to tableView

--- Project 09 - PullToRefresh/PullToRefresh/ViewController.swift
+++ Project 09 - PullToRefresh/PullToRefresh/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    // MARK: - TableView
+    
+    //Number of Sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Number of Rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // Display Data to TableView Cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "groceriesCell", for: indexPath)
+        
+        cell.textLabel?.text = "Hello"
+        
+        return cell
     }
 
 
