3e940e68ac3078913403f98e8fc37c942cf3b614
Joshua Okoro <joshuaojc@gmail.com>
2019-02-26 13:53:54 -0600

    Added Two TableViewController in main.storyboard with navigation controller and controller files.

---
+++ Project 14- Contacts/Contacts/Controller/HeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderTableViewCell.swift
+//  Contacts
+//
+//  Created by Joshua Okoro on 2/26/19.
+//  Copyright © 2019 Joshua Okoro. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Project 14- Contacts/Contacts/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Contacts
+//
+//  Created by Joshua Okoro on 2/26/19.
+//  Copyright © 2019 Joshua Okoro. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    
+    
+}
