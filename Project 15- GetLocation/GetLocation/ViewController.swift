0a95b7f2fd5d8165778efc927a1f989258d59d54
Joshua Okoro <joshuaojc@gmail.com>
2019-02-28 08:17:18 -0600

    Added core location manager and update function

--- Project 15- GetLocation/GetLocation/ViewController.swift
+++ Project 15- GetLocation/GetLocation/ViewController.swift
@@ -7,17 +7,44 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    let locationManager = CLLocationManager()
+    
+    // MARK: - Outlets
     @IBOutlet var locationLabel: UILabel!
     
 
+    // MARK: - Interactions
     @IBAction func locationButtonPressed(_ sender: UIButton) {
+        getLocation()
+    }
+    
+    func getLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        locationManager.stopUpdatingLocation()
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            if let placemarkData = placemarks {
+                let locationData = placemarkData[0]
+                print(locationData)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
     }
     
 }
