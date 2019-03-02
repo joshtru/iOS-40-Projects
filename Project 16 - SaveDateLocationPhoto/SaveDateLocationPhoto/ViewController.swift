5ada081a35b995004dec7c3fe9d9a0c199f465c5
Joshua Okoro <joshuaojc@gmail.com>
2019-03-02 15:50:26 -0600

    Added toolbar to keyboard

--- Project 16 - SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
+++ Project 16 - SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
@@ -12,9 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textView.inputAccessoryView = toolbar
+        textView.becomeFirstResponder()
+        
     }
-
-
+    // MARK: - Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var cameraImage: UIImageView!
+    @IBOutlet var toolbar: UIView!
+    
+    // MARK: - Interactions
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func cameraButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func locationButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
