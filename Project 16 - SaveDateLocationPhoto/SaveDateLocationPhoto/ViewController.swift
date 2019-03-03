946493b914a5f76e209ac1da2ea7f85f14710904
Joshua Okoro <joshuaojc@gmail.com>
2019-03-03 12:54:16 -0600

    Added photo upload functionality

--- Project 16 - SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
+++ Project 16 - SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +26,30 @@ class ViewController: UIViewController {
     // MARK: - Interactions
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
     }
+    
     @IBAction func cameraButtonPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
+        imagePicker.delegate = self
     }
+    
     @IBAction func locationButtonPressed(_ sender: UIButton) {
+    }
+    
+    // MARK: - Image Picker
+    let imagePicker = UIImagePickerController()
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            cameraImage.image = selectedImage
+            dismiss(animated: true, completion: nil)
+            textView.becomeFirstResponder()
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        textView.becomeFirstResponder()
     }
     
     
