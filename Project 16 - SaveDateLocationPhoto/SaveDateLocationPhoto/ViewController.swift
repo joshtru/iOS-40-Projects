da04ed4e198b30296b430d6b4d98afd7d05a3e2a
Joshua Okoro <joshuaojc@gmail.com>
2019-03-03 13:35:14 -0600

    Added location functionality

--- Project 16 - SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
+++ Project 16 - SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,9 +32,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.sourceType = .savedPhotosAlbum
         present(imagePicker, animated: true, completion: nil)
         imagePicker.delegate = self
+        locationManager.delegate = self
     }
     
     @IBAction func locationButtonPressed(_ sender: UIButton) {
+        getLocation()
     }
     
     // MARK: - Image Picker
@@ -51,6 +54,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         picker.dismiss(animated: true, completion: nil)
         textView.becomeFirstResponder()
     }
+    
+    // MARK: - Core Location
+    let locationManager = CLLocationManager()
+    
+    
+    func getLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.startUpdatingLocation()
+    }
+    
+    // MARK: CLLocation Manager Delegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            if let placemarkData = placemarks {
+                let locationData = placemarkData[0]
+                let city = locationData.locality!
+                let state = locationData.administrativeArea!
+                let country = locationData.isoCountryCode!
+                let zipCode = locationData.postalCode!
+                let location = "\(city), \(state) \(zipCode), \(country)"
+                self.locationLabel.text = location
+            } else {
+                print("Error: \(error?.localizedDescription ?? "Unknown Error")")
+            }
+        }
+    }
+    
+
     
     
     
